1846d5d3caacb55a7692b0e730b3459577d96833
Jan Kramer <6755350+j-kramer@users.noreply.github.com>
2024-06-21 11:19:16 +0200

    opdracht 11: use js strict mode, CONST styling & use style.display instead of style.visibility

--- dom-manipulatie/script.js
+++ dom-manipulatie/script.js
@@ -1,25 +1,23 @@
+"use strict";
+
 const heading = document.getElementById("heading");
-const myName = "j-kramer";
-const colors = ['red', 'green', 'blue', 'yellow', 'magenta'];
+const MY_NAME = "j-kramer";
+const COLORS = ['red', 'green', 'blue', 'yellow', 'magenta'];
 
 function changeHeading() {
     /* set text in the heading */
-    heading.innerHTML = "Hi " + myName + "!";
-    /* toggle visibility of the heading
-     *
-     * use style.visibility instead of style.display, since
-     * we do not need to remove the element from the dom
-     */
-    if (heading.style.visibility == 'hidden') {
-        heading.style.visibility = 'visible';
+    heading.innerHTML = "Hi " + MY_NAME + "!";
+    // toggle visibility of the heading
+    if (heading.style.display == 'none') {
+        heading.style.display = 'block';
     } else {
-        heading.style.visibility = 'hidden';
+        heading.style.display = 'none';
     }
     /* select a random color from the array
      *
      * Note that the random function returns a
      * right-open interval [0,1).
      */
-    let index = Math.floor(Math.random() * colors.length);
-    heading.style.color = colors[index];
+    let index = Math.floor(Math.random() * COLORS.length);
+    heading.style.color = COLORS[index];
 }
