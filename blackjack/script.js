520f16e7d23faf2769899d959eea968ccb62417e
Jan Kramer <6755350+j-kramer@users.noreply.github.com>
2024-09-06 12:15:01 +0200

    Blackjack: rewrite to use more classes

--- blackjack/script.js
+++ blackjack/script.js
@@ -1,9 +1,8 @@
 "use strict";
 
-const dealerScore = document.getElementById("dealerScore");
-const dealerCards = document.getElementById("dealerKaarten");
-const playerScore = document.getElementById("spelerScore");
-const playerCards = document.getElementById("spelerKaarten");
+const playerTemplate = document.getElementById("playerTemplate");
+const playersDiv = document.getElementById("blackjackPlayers");
+
 const dealCardButton = document.getElementById("deelButton");
 const passButton = document.getElementById("pasButton");
 
@@ -37,196 +36,243 @@ class Card {
     get value() {
         return this._value;
     }
-}
 
-
-/******************************************
- * Functions to update the html interface *
- ******************************************/
-
-function showScore(elem, score) {
-    elem.innerHTML = "Score: " + score;
-}
-
-function addCard(elem, card) {
-    let div = document.createElement('div');
-    div.className = "card";
-    div.innerHTML = (card.suit ?? "") + " " + (card.rank ?? "");
-    elem.append(div);
-}
-
-function showDealerHand() {
-    addCard(dealerCards, {});
-    addCard(dealerCards, game.dealerHand[1]);
-}
-
-function showHand(elem, hand) {
-    addCard(elem, hand[0]);
-    addCard(elem, hand[1]);
-}
-
-function updateButtons() {
-    if (game.started) {
-        dealCardButton.innerHTML = "Hit";
-        passButton.removeAttribute("disabled");
-    } else {
-        dealCardButton.innerHTML = "Delen";
-        passButton.setAttribute("disabled", "");
+    drawCard(elem, visible) {
+        let div = document.createElement('div');
+        div.className = "card";
+        if (visible) {
+            div.innerHTML = this._suit + " " + this._rank;
+        }
+        elem.append(div);
     }
 }
 
-function showPlayerWins() {
-    alert("You win");
-}
+class Player {
 
-function showPlayerLoses() {
-    alert("You lost");
-}
+    constructor(name) {
+        this._name = name;
+        this._hand = [];
 
-function showPlayerDraw() {
-    alert("Stand-off");
-}
+        /*
+         * add the Player to the view with a template,
+         * hence we don't need to pass the elements for the
+         * score to the constructor but can just select them
+         * here
+         */
+        const clone = playerTemplate.content.cloneNode(true);
 
-/*
- * User Callbacks
- */
+        let nameElem = clone.querySelector(".playerName");
+        nameElem.innerHTML = name + " Kaarten";
 
-function dealCardButtonCallback() {
-    if (game.started) {
-        hit();
-    } else {
-        delen();
+        this._scoreElem = clone.querySelector(".playerScore");
+        this._scoreElem.innerHTML = "Score:";
+
+        this._handElem = clone.querySelector(".playerHand");
+
+        playersDiv.append(clone);
+    }
+
+    addCard(card) {
+        this._hand.push(card);
+        this._calculateScore();
+        this._render();
+    }
+
+    get score() {
+        return this._score;
+    }
+
+    _calculateScore() {
+        let score = 0;
+        let nrOfAces = 0;
+        for (let card of this._hand) {
+            if (card.value == 11) {
+                nrOfAces++
+                continue;
+            }
+            score += card.value;
+        }
+        switch (nrOfAces) {
+            case 2:
+                if (score + 12 <= 21) {
+                    score += 12;
+                } else {
+                    score += 2;
+                }
+                break;
+            case 1:
+                if (score + 11 <= 21) {
+                    score += 11;
+                } else {
+                    score += 1;
+                }
+                break;
+            default:
+                score += nrOfAces;
+        }
+        this._score = score;
     }
 }
 
-function passTurn() {
-    if (game.started) {
-        pas();
+class HumanPlayer extends Player {
+    constructor() {
+        super("Speler");
     }
-}
 
-/*
- * Game Logic
- */
-
-const game = {
-    started: false,
-    deck: initDeck(),
-    dealerHand: [],
-    playerHand: [],
-}
-
-function initDeck() {
-    let deck = [];
-    const suits = ["diamonds", "clubs", "hearts", "spades"];
-    const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king", "ace"];
-    for (let i = 0; i < suits.length; i++) {
-        for (let j = 0; j < ranks.length; j++) {
-            deck.push(new Card(suits[i], ranks[j]));
+    _render() {
+        this._scoreElem.innerHTML = "Score: " + this._score;
+        this._handElem.innerHTML = '';
+        for (let card of this._hand) {
+            card.drawCard(this._handElem, true);
         }
     }
-    return deck;
 }
 
-function calculateScore(hand) {
-    let score = 0;
-    let hasAce = false;
-    for (let card of hand) {
-        if (card.value == 11) {
-            hasAce = true;
-            continue;
-        }
-        score += card.value;
+class DealerPlayer extends Player {
+    constructor() {
+        super("Dealer");
+        this._handIsVisible = false;
     }
-    if (hasAce) {
-        if (score + 11 > 21) {
-            score++;
+
+    setHandVisible() {
+        this._handIsVisible = true;
+        this._render();
+    }
+
+    _render() {
+        if (this._handIsVisible) {
+            this._scoreElem.innerHTML = "Score: " + this._score;
         } else {
-            score += 11;
+            this._scoreElem.innerHTML = "Score: hidden"
+        }
+
+        this._handElem.innerHTML = '';
+
+        let nrOfCards = this._hand.length;
+        if (nrOfCards > 0) {
+            this._hand[0].drawCard(this._handElem, this._handIsVisible);
+            for (let i = 1; i < nrOfCards; i++) {
+                this._hand[i].drawCard(this._handElem, true);
+            }
         }
     }
-    return score;
 }
 
-function drawCard(hand) {
-    let cardNr = Math.random() * game.deck.length;
-    let card = game.deck.splice(cardNr, 1)[0];
-    hand.push(card);
-    return card;
-}
-
-function delen() {
-    // reset the game
-    resetGame();
-
-    drawCard(game.dealerHand);
-    drawCard(game.dealerHand);
-
-    drawCard(game.playerHand);
-    drawCard(game.playerHand);
-
-    showDealerHand();
-    showHand(playerCards, game.playerHand);
-    showScore(dealerScore, "");
-    showScore(playerScore, calculateScore(game.playerHand));
-}
-
-function resetGame() {
-    game.started = true;
-    game.deck = initDeck();
-    game.dealerHand = [];
-    game.playerHand = [];
-
-    // Clear existing hands
-    dealerCards.innerHTML = '';
-    playerCards.innerHTML = '';
-
-    updateButtons();
-}
-
-function hit() {
-    addCard(playerCards, drawCard(game.playerHand));
-    let score = calculateScore(game.playerHand);
-    showScore(playerScore, score);
-    if (score == 21) {
-        game.started = false;
-        showPlayerWins();
+class Blackjack {
+    constructor() {
+        this._setCanStart(true);
+        this._dealer = new DealerPlayer();
+        this._player = new HumanPlayer();
     }
-    if (score > 21) {
-        game.started = false;
-        showPlayerLoses();
-    }
-    updateButtons();
-}
 
-function pas() {
-    game.started = false;
-    // Start the dealers turn
-    dealerCards.innerHTML = '';
-    showHand(dealerCards, game.dealerHand);
-    let score = calculateScore(game.dealerHand);
-    let playerScore = calculateScore(game.playerHand);
-    while (score < 17) {
-        if (score == 16 && playerScore == 16) {
-            break;
+    _reset() {
+        this._deck = this._initDeck();
+        playersDiv.innerHTML = '';
+        this._dealer = new DealerPlayer();
+        this._player = new HumanPlayer();
+    }
+
+    _initDeck() {
+        let deck = [];
+        const suits = ["diamonds", "clubs", "hearts", "spades"];
+        const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king", "ace"];
+        for (let i = 0; i < suits.length; i++) {
+            for (let j = 0; j < ranks.length; j++) {
+                deck.push(new Card(suits[i], ranks[j]));
+            }
         }
-        addCard(dealerCards, drawCard(game.dealerHand));
-        score = calculateScore(game.dealerHand);
+        return deck;
     }
-    showScore(dealerScore, score);
-    if (score > 21 || score < playerScore) {
-        showPlayerWins();
+
+    _drawCard(player) {
+        let cardNr = Math.random() * this._deck.length;
+        let card = this._deck.splice(cardNr, 1)[0];
+        player.addCard(card);
+    }
+
+    _setCanStart(canStart) {
+        this._canStart = canStart;
+        if (canStart) {
+            dealCardButton.innerHTML = "Delen";
+            passButton.setAttribute("disabled", "");
+        } else {
+            dealCardButton.innerHTML = "Hit";
+            passButton.removeAttribute("disabled");
+
+        }
+    }
+
+    canStart() {
+        return this._canStart;
+    }
+
+    start() {
+        this._reset();
+
+        this._drawCard(this._dealer);
+        this._drawCard(this._dealer);
+
+
+        this._drawCard(this._player);
+        this._drawCard(this._player);
+
+        this._setCanStart(false);
+    }
+
+    hit() {
+        this._drawCard(this._player);
+        let score = this._player.score;
+        if (score == 21) {
+            this._endGame("You win");
+        }
+        if (score > 21) {
+            this._endGame("You lose");
+        }
+    }
+
+    pas() {
+        let dealer = this._dealer;
+
+        dealer.setHandVisible();
+
+        let score = dealer.score;
+        let playerScore = this._player.score;
+        while (score < 17) {
+            if (score == 16 && playerScore == 16) {
+                break;
+            }
+            this._drawCard(dealer);
+            score = dealer.score;
+        }
+        if (score > 21 || score < playerScore) {
+            this._endGame("You win");
+        } else {
+            if (score > playerScore) {
+                this._endGame("You lose");
+            }
+            if (score == playerScore) {
+                this._endGame("Stand-off");
+            }
+        }
+    }
+
+    _endGame(message) {
+        alert(message);
+        this._setCanStart(true);
+    }
+}
+
+let game = new Blackjack();
+
+dealCardButton.addEventListener("click", () => {
+    if (game.canStart()) {
+        game.start();
     } else {
-        if (score > playerScore) {
-            showPlayerLoses();
-        }
-        if (score == playerScore) {
-            showPlayerDraw();
-        }
+        game.hit();
     }
-    updateButtons();
-}
+});
 
-// add callbacks
-dealCardButton.addEventListener("click", dealCardButtonCallback);
-passButton.addEventListener("click", passTurn);
+// the closure is needed to ensure so this refers to the game instead of the button
+passButton.addEventListener("click", () => {
+    game.pas()
+});
