abf333de346fb9d043bbc61f3494924b73ba6835
Jan Kramer <6755350+j-kramer@users.noreply.github.com>
2024-10-15 15:43:37 +0200

    laravel-blog: change logo

--- laravel-blog/resources/views/layouts/guest.blade.php
+++ laravel-blog/resources/views/layouts/guest.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="shortcut icon" href="{{ asset('y.svg') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
