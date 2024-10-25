1748701de3d569195ee1c143b499ca5e9d716754
j-kramer <6755350+j-kramer@users.noreply.github.com>
2024-10-25 14:29:32 +0200

    laravel-blog: add category selector to homepage

--- laravel-blog/app/Http/Controllers/HomeServer.php
+++ laravel-blog/app/Http/Controllers/HomeServer.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Article;
+use App\Models\Category;
 
 class HomeServer extends Controller
 {
@@ -13,8 +14,17 @@ class HomeServer extends Controller
      */
     public function __invoke(Request $request)
     {
+        $selectedCategoryID = $request->query("category");
+        if (isset($selectedCategoryID)) {
+            $articles =  Category::find($selectedCategoryID)->articles()->with(['user:id,name', 'categories:id,name'])->latest()->paginate(10);
+        } else {
+            $articles =  Article::with(['user:id,name', 'categories:id,name'])->latest()->paginate(10);
+        }
+
         return view('home', [
-            'articles' => Article::with(['user:id,name', 'categories:id,name'])->latest()->paginate(10),
+            'articles' => $articles,
+            'categories' => Category::all(),
+            'selectedCategoryID' => $selectedCategoryID,
         ]);
     }
 }
--- laravel-blog/resources/views/home.blade.php
+++ laravel-blog/resources/views/home.blade.php
@@ -1,5 +1,18 @@
 <x-app-layout>
-    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+    <div class="max-w-2xl mx-auto p-0 sm:p-6 lg:p-8">
+        <form method="GET">
+            <div class="flex justify-between items-center">
+                <x-input-label for="category" :value="__('Category:')" />
+                <select name="category" id="category" onchange="this.form.submit();">
+                    <option value="">All</option>
+                    @foreach($categories as $category)
+                    <option value="{{ $category->id }}" {{ $selectedCategoryID == $category->id ? 'selected' : '' }}>
+                        {{ $category->name }}
+                    </option>
+                    @endforeach
+                </select>
+            </div>
+        </form>
         <x-article-list :$articles />
     </div>
     <footer class="py-16 text-center text-sm text-black">
