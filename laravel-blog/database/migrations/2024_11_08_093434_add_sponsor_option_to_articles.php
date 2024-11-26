f941efcde0206405a89a5b3484ae912aec5c7171
Jan Kramer <6755350+j-kramer@users.noreply.github.com>
2024-11-26 15:40:54 +0100

    laravel-blog: remove sponsored_untill nullable modifier

--- laravel-blog/database/migrations/2024_11_08_093434_add_sponsor_option_to_articles.php
+++ laravel-blog/database/migrations/2024_11_08_093434_add_sponsor_option_to_articles.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('articles', function (Blueprint $table) {
-            $table->dateTime('sponsored_untill')->nullable();
+            $table->dateTime('sponsored_untill');
         });
     }
 
