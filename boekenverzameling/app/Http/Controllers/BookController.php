08294ecdfc478ebe4a1d3edeac918e73fe5b288b
j-kramer <6755350+j-kramer@users.noreply.github.com>
2024-12-20 12:28:38 +0100

    boekverzameling: add basic book api/model

---
+++ boekenverzameling/app/Http/Controllers/BookController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBookRequest;
+use App\Http\Requests\UpdateBookRequest;
+use App\Http\Resources\BookResource;
+use App\Models\Book;
+
+use Illuminate\Support\Facades\Storage;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $books = Book::all();
+        return BookResource::collection($books);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBookRequest $request)
+    {
+        $validated = $request->validated();
+        $validated['cover_path'] = $validated['cover']->store('images');
+        $book = Book::Create($validated);
+        return (new BookResource($book))->response()->setStatusCode(201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Book $book)
+    {
+        return new BookResource($book);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBookRequest $request, Book $book)
+    {
+        $validated = $request->validated();
+        Storage::delete($book->cover_path);
+        $validated['cover_path'] = $validated['cover']->store('images');
+        $book->update($validated);
+        return new BookResource($book);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Book $book)
+    {
+        $book->delete();
+        return response()->json(null, 204);
+    }
+}
---
+++ boekenverzameling/app/Models/Book.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    /** @use HasFactory<\Database\Factories\BookFactory> */
+    use HasFactory;
+    use HasUuids;
+
+    protected $fillable = [
+        'title',
+        'cover_path',
+    ];
+}
