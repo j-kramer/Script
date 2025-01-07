f64ab03c714b0c417b889c04794fe5d2d341cc6e
Jan Kramer <6755350+j-kramer@users.noreply.github.com>
2025-01-07 15:44:32 +0100

    show image in booklist

--- boekenverzameling/resources/js/domains/books/components/BookList.vue
+++ boekenverzameling/resources/js/domains/books/components/BookList.vue
@@ -3,8 +3,21 @@ const props = defineProps<{
     books: Book[];
 }>();
 </script>
+
 <template>
     <ul>
-        <li v-for="book in books">{{ book.cover_path }} -{{ book.title }}</li>
+        <li v-for="(book, index) in books" :key="index">
+            <div>
+                <img :src="'/' + book.cover_path" alt="book cover">
+            </div>
+            {{ book.title }}
+        </li>
     </ul>
 </template>
+
+<style lang="css" scoped>
+img {
+    width: 5rem;
+    height: 5rem;
+}
+</style>
