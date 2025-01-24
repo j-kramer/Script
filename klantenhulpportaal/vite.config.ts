765dab2cceefce5534b36a226303f3e9c3b8b89d
j-kramer <6755350+j-kramer@users.noreply.github.com>
2025-01-24 10:03:08 +0100

    Klantenhulpportaal: add eslint, vueDevTools

--- klantenhulpportaal/vite.config.ts
+++ klantenhulpportaal/vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.ts'],
+            input: ["resources/css/app.css", "resources/js/app.ts"],
             refresh: true,
         }),
         vue(),
+        vueDevTools({
+            appendTo: "resources/js/app.ts",
+        }),
     ],
 });
