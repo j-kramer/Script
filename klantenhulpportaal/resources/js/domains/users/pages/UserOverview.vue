51baf2b52cb7d1652f41b48df9efc63674225c8c
j-kramer <6755350+j-kramer@users.noreply.github.com>
2025-02-21 14:59:19 +0100

    klantenhulpportaal: add user deletion

--- klantenhulpportaal/resources/js/domains/users/pages/UserOverview.vue
+++ klantenhulpportaal/resources/js/domains/users/pages/UserOverview.vue
@@ -1,7 +1,23 @@
 <script setup lang="ts">
+import type {User} from '../types';
+
+import Delete from 'components/icons/Delete.vue';
+import {confirmModal} from 'services/modal';
+import {successToast} from 'services/toast';
+
 import {userStore} from '..';
 
 userStore.actions.getAll();
+
+const deleteUser = async function (user: User) {
+    const confirmed = await confirmModal(
+        `Weet je zeker dat je ${user.firstName} ${user.lastName} wilt verwijderen?`,
+        'Verwijderen',
+    );
+    if (!confirmed) return;
+    userStore.actions.delete(user.id);
+    successToast('Gebruiker verwijderd');
+};
 </script>
 
 <template>
@@ -13,6 +29,7 @@ userStore.actions.getAll();
                 <th class="text-left">E-mail</th>
                 <th class="text-left">Telefoonnummer</th>
                 <th class="text-left">Rol</th>
+                <th>Acties</th>
             </tr>
         </thead>
         <tbody>
@@ -22,6 +39,9 @@ userStore.actions.getAll();
                 <td class="text-left">{{ user.email }}</td>
                 <td class="text-left">{{ user.phonenumber }}</td>
                 <td class="text-left">{{ user.isAdmin ? 'Admin' : 'Gebruiker' }}</td>
+                <td class="flex flex-row justify-center">
+                    <button @click="deleteUser(user)"><Delete /></button>
+                </td>
             </tr>
         </tbody>
     </table>
